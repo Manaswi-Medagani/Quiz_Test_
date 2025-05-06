821ad4b1543e9c48b5aa314ea2effda6d60293cb
manaswi medagani <medaganimanaswi@gmail.com>
2025-05-06 20:23:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -390,4 +390,4 @@ def server_error(error):
     return jsonify({'error': 'Internal server error'}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, host='127.0.0.1', port=5000)
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
